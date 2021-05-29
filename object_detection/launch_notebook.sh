eb27cb6f27d71f1a6ccee1800e9f3e6e9406ab0a
navubeast <84241882+navubeast@users.noreply.github.com>
2021-05-29 21:45:39 +0530

    Update launch_notebook.sh

--- object_detection/launch_notebook.sh
+++ object_detection/launch_notebook.sh
@@ -1,14 +1,14 @@
 # Fill in the first and last name you use to register
 # in lower case letters.
-export FIRST_NAME=[fill_in]
-export LAST_NAME=[fill_in]
+export FIRST_NAME=[navtesh]
+export LAST_NAME=[batra]
 
 # You will be given the determined cluster URL once the
 # webinar starts.
 export DET_MASTER=[fill_in]
 # Once you get the determined cluster URL, you can login 
 # with your account.
-det user login ${FIRST_NAME}_${LAST_NAME}
+det user login ${navtesh}_${batra}
 # password is blank initially.  you can change it by running
 #det user change-password
 
